1e258b312bd7496283f0b32d954a834de8acd358
enewen0324 <shift9969.cs07@nctu.edu.tw>
2020-09-17 16:32:38 +0800

    visual comfort

--- wgtest.sh
+++ wgtest.sh
@@ -7,11 +7,11 @@ key_path="/home/andy/wgkey/"
 server_key_path="/home/andy/wg/"
 wgconf_path="/usr/local/etc/wireguard/"
 
-read -p "The wireguard configure you want to change:" wg_name
-read -p "Action:(add or delete)" wg_action
+read -p "\nThe wireguard configure you want to change: " wg_name
+read -p "\nAction(add or delete): " wg_action
 case $wg_action in
     "add")
-        read -p "hostname:" wg_hostname
+        read -p "\nhostname: " wg_hostname
         cd $key_path
         mkdir $wg_hostname
         cd $wg_hostname
@@ -20,24 +20,23 @@ case $wg_action in
         wg_private=$(cat privatekey) 
         wg_reg_pub=$(cat publickey | sed -e "s/\\//\\\\\//g")
         wg_reg_pri=$(cat privatekey | sed -e "s/\\//\\\\\//g")
-        echo $wg_reg_pub
-        echo $wg_reg_pri
         ip_number="$(cat ${wgconf_path}${wg_name}.conf | grep Peer | wc -l)"
         ip_number=$(( ${ip_number} + 2 ))
-        echo "192.168.50.$ip_number"
         touch "192.168.50.$ip_number"
         sed -e "s/A.B.C.D/192.168.50.${ip_number}\/32/g" -e "s/KKKEY/${wg_reg_pub}/g" $template_path$template_name >> "${wgconf_path}${wg_name}.conf"
         printf "\n\n" >> "${wgconf_path}${wg_name}.conf"
-        echo "Your publickey"
+        printf "\nYour publickey:\n"
         echo $wg_public
-        echo "Your tunnel file"
         sed -e "s/A.B.C.D/192.168.50.${ip_number}\/24/g" -e "s/PPPRIKEY/${wg_reg_pri}/g" $template_path$template_tunnel_name > "${key_path}${wg_hostname}/tunnel"
+        printf "\nYour tunnel file\n"
+        printf "----------------------------"
         cat ${key_path}${wg_hostname}/tunnel
+        printf "----------------------------\n"
     ;;
     "delete")
     ;;
 esac
-read -p "Do you want to restart?(y/N)" restart_op
+read -p "Do you want to restart?(y/N) " restart_op
 if [ $restart_op == "y" ];then
     wg-quick down $wg_name
     wg-quick up $wg_name
