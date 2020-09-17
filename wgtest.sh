942be7da9f28239c284834ddd234112f2eaa7a64
enewen0324 <shift9969.cs07@nctu.edu.tw>
2020-09-17 15:22:54 +0800

    fixed bug

--- wgtest.sh
+++ wgtest.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-template_path="/home/andy/wg_tmeplate"
+
+template_path="/home/andy/wireguard_auto_create/"
 template_name="wg_tmeplate.txt"
+template_tunnel_name="wg_tunnel_tmeplate.txt"
 key_path="/home/andy/wgkey/"
 server_key_path="/home/andy/wg/"
 wgconf_path="/usr/local/etc/wireguard/"
@@ -20,11 +22,11 @@ case $wg_action in
         echo $ip_number
         echo "192.168.50.$(( ${ip_number} + 2 ))"
         touch "192.168.50.$(( ${ip_number} + 2 ))"
-        sed -e "s/A.B.C.D/192.168.50.${ip_number}\/32/g" -e "s/KKKEY/${wg_public}/g" wg_tmeplate >> ${wgconf_path}${wg_name}
+        sed -e "s/A.B.C.D/192.168.50.${ip_number}\/32/g" -e "s/KKKEY/${wg_public}/g" $template_path$template_name >> ${wgconf_path}${wg_name}
         echo "Your publickey"
         echo $wg_public
         echo "Your tunnel file"
-        sed -e "s/A.B.C.D/192.168.50.${ip_number}\/24/g" -e "s/PPPRIKEY/${wg_private}/g" wg_tmeplate > ${key_path}${$wg_hostname}/tunnel
+        sed -e "s/A.B.C.D/192.168.50.${ip_number}\/24/g" -e "s/PPPRIKEY/${wg_private}/g" $template_path$template_tunnel_name > ${key_path}${$wg_hostname}/tunnel
         cat ${key_path}${$wg_hostname}/tunnel
     ;;
     "delete")
