31cab67ef2a6d54af54f3a3e59471d23d4539a40
enewen0324 <shift9969.cs07@nctu.edu.tw>
2020-09-18 14:46:50 +0800

    delete function and fixed ip porblem

--- wgtest.sh
+++ wgtest.sh
@@ -6,9 +6,22 @@ template_tunnel_name="wg_tunnel_template.txt"
 key_path="/home/andy/wgkey/"
 server_key_path="/home/andy/wg/"
 wgconf_path="/usr/local/etc/wireguard/"
+backup_path=""
+max_backup_num="5"
+
+work_dir=$(pwd)
 
 read -p "The wireguard configure you want to change: " wg_name
-read -p "Action(add or delete): " wg_action
+read -p "Action(add or delete or recover): " wg_action
+
+date=$(date +"%F#%T")
+cp "$wgconf_path$wg_name.conf" backup/$date
+backup_num=$(ls backup | wc -l)
+if [ $backup_num lt $max_backup_num ];then
+    delete_file=$(ls backup | head -1)
+    echo -e "delete ${delete_file}"
+fi
+
 case $wg_action in
     "add")
         read -p "hostname: " wg_hostname
@@ -20,10 +33,16 @@ case $wg_action in
         wg_private=$(cat privatekey) 
         wg_reg_pub=$(cat publickey | sed -e "s/\\//\\\\\//g")
         wg_reg_pri=$(cat privatekey | sed -e "s/\\//\\\\\//g")
-        ip_number="$(cat ${wgconf_path}${wg_name}.conf | grep Peer | wc -l)"
-        ip_number=$(( ${ip_number} + 2 ))
+
+        #need to change
+        ip_number="$(cat $work_dir/.${wg_name} | head -1 | sed -e "s/@//g" )"
         touch "192.168.50.$ip_number"
+        sed -i -e "/$ip_number/,+d"  $work_dir/.${wg_name}
+        #need to change
+
+        echo "#${wg_hostname}" >> "${wgconf_path}${wg_name}.conf"
         sed -e "s/A.B.C.D/192.168.50.${ip_number}\/32/g" -e "s/KKKEY/${wg_reg_pub}/g" $template_path$template_name >> "${wgconf_path}${wg_name}.conf"
+        
         echo -e  "\n\n" >> "${wgconf_path}${wg_name}.conf"
         echo -e "\nYour publickey:\n"
         echo $wg_public
@@ -34,6 +53,18 @@ case $wg_action in
         echo -e "----------------------------\n"
     ;;
     "delete")
+        read -p "host you want to delete: " wg_hostname
+        ip_number="$( ls $key_path$wg_hostname | grep -e "[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}" | cut -d . -f 4)"
+        echo $ip_number
+        sed -i -e "/#$wg_hostname/,+4d" "${wgconf_path}${wg_name}.conf"
+        rm -rf $key_path$wg_hostname
+        echo "@$ip_number@" >>　$work_dir/.${wg_name}
+    ;;
+    "recover")
+
+    ;;
+    "create")
+    
     ;;
     "test")
         printf "\nYour tunnel file\n"
